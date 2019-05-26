08aa74dc03584ef8056e1c988130502ac48da782
Diana Arcanjo <dianaarcanjo.dev@gmail.com>
2019-05-26 12:36:26 -0300

    commit do app Gerador de Numeros Randomicos

--- GeradorRandomicoAPP/ViewController.swift
+++ GeradorRandomicoAPP/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        let numero = arc4random_uniform(10)
+        legendaResultado.text = String(numero)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
